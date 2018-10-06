b73560af1bea149acd3b5aeecda22ad4f42fa866
Daniel Koller <daniel@dakoller.net>
2018-10-06 20:52:49 +0530

    integrated secretfile & ini file reader

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 )
 
 func main() {
+	cfg, err := ini.Load("secretfile")
+        if err != nil {
+            fmt.Printf("Fail to read file: %v", err)
+            os.Exit(1)
+        }
+
+        // Classic read of values, default section can be represented as empty string
+        fmt.Println("App Mode:", cfg.Section("").Key("app_mode").String())
+        fmt.Println("Data Path:", cfg.Section("paths").Key("data").String())	
+
+
 	resp, err := http.Get(os.Getenv("URL"))
 	check(err)
 	body, err := ioutil.ReadAll(resp.Body)
