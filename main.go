8128614b5674fb24bf61a2504479453ac574d9bc
Daniel Koller <daniel@dakoller.net>
2018-10-06 20:44:52 +0530

    ini reader included

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+        "gopkg.in/ini.v1"
 )
 
 func main() {
-    resp, err := http.Get(os.Getenv("URL"))
-    check(err)
-    body, err := ioutil.ReadAll(resp.Body)
-    check(err)
-    fmt.Println(os.Getenv("URL"))
-    fmt.Println(len(body))
+	resp, err := http.Get(os.Getenv("URL"))
+	check(err)
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+	fmt.Println(os.Getenv("URL"))
+	fmt.Println(len(body))
 }
 
 func check(err error) {
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
